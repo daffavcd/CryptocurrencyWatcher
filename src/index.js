f26169d552ef4c9771d2d2655451492be5a10f5d
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-03-09 14:17:05 +0700

    changing findPercentage by asycronously

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@
 import jQuery from 'jquery';
 window.$ = jQuery;
 
-import 'bootstrap';
-import './scss/app.scss';
-import './css/style.css';
-
+import "@fontsource/open-sans";
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+import 'bootstrap';
+import './scss/app.scss';
+import './css/style.css';
+
